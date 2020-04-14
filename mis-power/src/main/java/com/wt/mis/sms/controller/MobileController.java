5609241166e962fe9ff55d42c82f36abe38cb01d
wangtao <xlwt2113@hotmail.com>
2020-04-14 23:53:27 +0800

    增加短信设置

--- mis-power/src/main/java/com/wt/mis/sms/controller/MobileController.java
+++ mis-power/src/main/java/com/wt/mis/sms/controller/MobileController.java
@@ -2,8 +2,11 @@
 package com.wt.mis.sms.controller;
 
 import com.wt.mis.core.controller.BaseController;
+import com.wt.mis.core.dao.PageResult;
 import com.wt.mis.core.repository.BaseRepository;
+import com.wt.mis.core.service.SearchService;
 import com.wt.mis.core.util.LoginUser;
+import com.wt.mis.core.util.ResponseUtils;
 import com.wt.mis.core.util.StringUtils;
 import com.wt.mis.dev.entity.Line;
 import com.wt.mis.dev.entity.Topology;
@@ -14,14 +17,15 @@ import com.wt.mis.dev.repository.TransFormRepository;
 import com.wt.mis.sms.entity.JsonTreeModel;
 import com.wt.mis.sms.entity.Mobile;
 import com.wt.mis.sms.repository.MobileRepository;
+import com.wt.mis.sys.entity.Account;
 import com.wt.mis.sys.entity.Dep;
 import com.wt.mis.sys.repository.DepRespository;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
@@ -47,6 +51,9 @@ public class MobileController extends BaseController<Mobile> {
     @Autowired
     DepRespository depRespository;
 
+    @Autowired
+    SearchService searchService;
+
     @Override
     public BaseRepository<Mobile, Long> repository() {
         return mobileRepository;
@@ -59,18 +66,41 @@ public class MobileController extends BaseController<Mobile> {
 
     @Override
     protected String generateSearchSql(Mobile mobile, HttpServletRequest request) {
+        StringBuffer sql = new StringBuffer(" select t1.*,t2.mobile,t2.real_name,t3.transform_name from sms_mobile as t1  left join sys_account t2 on t1.account_id = t2.id left join dev_transform t3 on t1.transform_id = t3.id where t1.del = 0 ");
+        if (StringUtils.isNotEmpty(mobile.getMobile())) {
+            sql.append(" and t2.mobile like '%" + mobile.getMobile() + "%'");
+        }
+        if (StringUtils.isNotEmpty(mobile.getName())) {
+            sql.append(" and t2.realName like '%" + mobile.getName() + "%'");
+        }
+        return sql.toString();
+    }
+
+
+    @ApiOperation("根据对象的属性查询所有对象")
+    @PostMapping("/list_user")
+    @ResponseBody
+    protected ResponseEntity list(HttpServletRequest request,
+                                  Account account,
+                                  @RequestParam(value = "page", required = false, defaultValue = "1") Integer pageNumber,
+                                  @RequestParam(value = "limit", required = false, defaultValue = "15") Integer pageSize,
+                                  @RequestParam(value = "pageSort", required = false, defaultValue = "id") String pageSort,
+                                  @RequestParam(value = "pageDirection", required = false, defaultValue = "DESC") String pageDirection) {
+
         String devId = request.getParameter("devId");
-        Dep dep = depRespository.getOne(LoginUser.getCurrentUser().getDepId());
         StringBuffer sql = new StringBuffer("select t1.*,t2.`name` as dep_name from sys_account t1 left join sys_dep t2  on t1.dep_id = t2.id  where t1.del = 0 ");
         //默认查看当前登录人归属部门及下级部门的
         if(StringUtils.isNotEmpty(devId)){
             TransForm transForm = transFormRepository.getOne(Long.parseLong(devId));
-            dep = depRespository.getOne(transForm.getOperationsTeam());
+            Dep dep = depRespository.getOne(transForm.getOperationsTeam());
+            sql.append(" and t2.level like '" + dep.getLevel() + "%' " );
+            PageResult page = searchService.findBySql(sql.toString(), pageNumber, pageSize);
+            dealSearchList(page.getContent());
+            return ResponseUtils.ok("", page);
+        }else{
+            return ResponseUtils.ok("", new PageResult());
         }
-        sql.append(" and t2.level like '" + dep.getLevel() + "%' " );
-        return sql.toString();
     }
-
 
     @ResponseBody
     @GetMapping("line_tree")
@@ -98,5 +128,21 @@ public class MobileController extends BaseController<Mobile> {
             treeData.add(jsonLine);
         }
         return treeData;
+    }
+
+    @Override
+    @ApiOperation("提交创建对象")
+    @PostMapping("/add")
+    @ResponseBody
+    protected String add(HttpServletRequest request, Mobile mobile) {
+        String[] chooseAccountIds = request.getParameter("chooseAccountId").split(",");
+        String chooseTransformId = request.getParameter("chooseTransformId");
+        for(String chooseAccountId : chooseAccountIds){
+            Mobile mobile1 = new Mobile();
+            mobile1.setAccountId(Long.parseLong(chooseAccountId));
+            mobile1.setTransformId(Long.parseLong(chooseTransformId));
+            this.mobileRepository.save(mobile1);
+        }
+        return ResponseUtils.okJson("新增成功", mobile);
     }
 }
