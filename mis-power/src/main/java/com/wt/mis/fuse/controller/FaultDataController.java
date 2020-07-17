1774b980ad39a85c7c8bf36e683ee85b21cf0309
wangtao <xlwt2113@hotmail.com>
2020-07-17 22:50:47 +0800

    增加权限

--- mis-power/src/main/java/com/wt/mis/fuse/controller/FaultDataController.java
+++ mis-power/src/main/java/com/wt/mis/fuse/controller/FaultDataController.java
@@ -3,6 +3,7 @@ package com.wt.mis.fuse.controller;
 
 import com.wt.mis.core.controller.BaseController;
 import com.wt.mis.core.repository.BaseRepository;
+import com.wt.mis.core.util.LoginUser;
 import com.wt.mis.core.util.StringUtils;
 import com.wt.mis.fuse.entity.FaultData;
 import com.wt.mis.fuse.repository.FaultDataRepository;
@@ -50,13 +51,24 @@ public class FaultDataController extends BaseController<FaultData> {
     @RequestMapping("/event_types")
     @ResponseBody
     public List<Node> dictJson(){
+        boolean hasRole = LoginUser.hasRole("/fuse/faultdata/list","viewAll");
         Dict dict = dictRepository.getFirstByDictNameAndDel("熔断器事件类型",0);
         List<DictItem> list = dictItemRepository.getAllByDictAndDel(dict,0);
         List nodeList = new ArrayList();
         for(DictItem item:list){
             if(!item.getItemValue().equals("105")&&!item.getItemValue().equals("305")&&!item.getItemValue().equals("205")){
-                Node node = new Node(item.getItemValue(),item.getItemKey());
-                nodeList.add(node);
+                if(hasRole){
+                    //能查看全部数据
+                    Node node = new Node(item.getItemValue(),item.getItemKey());
+                    nodeList.add(node);
+                }else{
+                    //不能查看106，206，306的数据
+                    if(!item.getItemValue().equals("106")&&!item.getItemValue().equals("206")&&!item.getItemValue().equals("306")){
+                        Node node = new Node(item.getItemValue(),item.getItemKey());
+                        nodeList.add(node);
+                    }
+                }
+
             }
         }
         return nodeList;
@@ -91,7 +103,12 @@ public class FaultDataController extends BaseController<FaultData> {
 
     @Override
     protected String generateSearchSql(FaultData faultData, HttpServletRequest request) {
+        boolean hasRole = LoginUser.hasRole("/fuse/faultdata/list","viewAll");
+
         StringBuffer sql = new StringBuffer("select t1.*,t2.hub_location,t2.hub_address from fuse_fault_data as t1 left join fuse_dev_hub as t2 on t1.hub_id = t2.id where t1.del = 0 ");
+        if(!hasRole){
+            sql.append(" and (t1.event_type !=106  and t1.event_type!=206 and t1.event_type != 306)");
+        }
         if (StringUtils.isNotEmpty(request.getParameter("hubName"))) {
             sql.append(" and t2.hub_location like '%" + request.getParameter("hubName") + "%'");
         }
